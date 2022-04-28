e1e262d9863a0c72584e4f1c81c76609d518047c
Rick Ertl <ertl.rick@gmail.com>
2022-04-28 16:07:21 -0500

    added read icon and color based on status

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const listLibrary = function () {
       <th>Title</th>
       <th>Author</th>
       <th>Pages</th>
-      <th>Read</th>
+      <th></th>
       <th></th>
     </tr>
   </thead>
@@ -70,13 +70,22 @@ const listLibrary = function () {
     title.textContent = element.title;
     author.textContent = element.author;
     pages.textContent = element.pages;
-    read.textContent = element.read === "true" ? "read" : "not read yet";
+    read.innerHTML = `
+    <button type="button">
+      <svg style="color: ${element.read === "true" ? "green" : "silver"};" 
+      viewBox="0 0 24 24">
+        <path fill="currentColor" d="M21.59,11.59L23,13L13.5,22.5L8.42,17.41L9.83,16L13.5,19.68L21.59,11.59M4,16V3H6L9,3A4,4 0 0,1 13,7C13,8.54 12.13,9.88 10.85,10.55L14,16H12L9.11,11H6V16H4M6,9H9A2,2 0 0,0 11,7A2,2 0 0,0 9,5H6V9Z" />
+      </svg>
+    </button>
+    `;
     remove.innerHTML = `
-      <button type="button" class="delete-btn" data-cmd="delete" data-id="${myLibrary.indexOf(
-        element
-      )}"><svg viewBox="0 0 24 24">
-      <path fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
-  </svg></button>
+    <button type="button" class="delete-btn" data-cmd="delete" data-id="${myLibrary.indexOf(
+      element
+    )}">
+      <svg viewBox="0 0 24 24">
+        <path fill="currentColor" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
+      </svg>
+    </button>
     `;
   });
   table.appendChild(tableBody);
